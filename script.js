5bffaa7996a1355b17c8762622807a1b4bf2d890
orysia1903 <orysia.pirko991@gmail.com>
2021-04-20 22:06:57 +0300

    C:/Program Files/Git/

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ function init() {
         displayBlock.classList.add('hide');
    
         if (count < 9) {
-            count++
-            previous.classList.remove('ends')
+            count++;
+            previous.classList.remove('ends');
         } else {
-            next.classList.add('ends')
+            next.classList.add('ends');
         }
         
         fetch(url)
@@ -31,7 +31,7 @@ function init() {
                 return resp.json();
             }).then(function (data) {
                 for (var i = 0; i < data.results.length; i++) {
-                    showPerson[i].innerHTML = data.results[i].name
+                    showPerson[i].innerHTML = data.results[i].name;
                     showPerson[i].addEventListener('click', showPersons);
 
                     function showPersons() {
@@ -43,44 +43,48 @@ function init() {
                             filmsTitles = document.querySelector('#films'),
                             species = document.querySelector('#species');
                         
-                        hero.classList.add('display-hero')   
+                        hero.classList.add('display-hero');
 
-                        name.innerHTML = target.innerHTML
+                        name.innerHTML = target.innerHTML;
                         for (var j = 0; j < data.results.length; j++) {
 
                             if (target.innerHTML == data.results[j].name) {
-                                gender.innerHTML = data.results[j].gender
-                                birthYear.innerHTML = data.results[j].birth_year
+                                gender.innerHTML = data.results[j].gender;
+                                birthYear.innerHTML = data.results[j].birth_year;
+                                
+                                var dom = data.results[j].homeworld;
+                                var arrayLinkPlanet = dom.split(':');
+                                arrayLinkPlanet.splice(1, 0, 's');
+                                arrayLinkPlanet.join('');
 
-                                fetch(data.results[j].homeworld)
+                                fetch(arrayLinkPlanet)
                                     .then(function (home) {
-                                        return home.json()
+                                        return home.json();
                                     }).then(function (planet) {
-                                        homeworld.innerHTML = planet.name
+                                        
+                                        homeworld.innerHTML = planet.name;
                                     })
 
-                                if (data.results[j].species.length > 0){
-                                    fetch(data.results[j].species[0])
-                                    .then(function (view) {
-                                            return view.json()
-                                    }).then(function (kind) {
-                                        species.innerHTML = kind.name
-                                    })
-                                } else {
-                                    species.innerHTML = 'unknown'
-                                }
+                                // if (data.results[j].species.length > 0){
+                                //     fetch(data.results[j].species[0])
+                                //     .then(function (view) {
+                                //             return view.json()
+                                //     }).then(function (kind) {
+                                //         species.innerHTML = kind.name;
+                                //     })
+                                // } else {
+                                //     species.innerHTML = 'unknown';
+                                // }
 
-                                for(var k = 0; k < data.results[j].films.length; k++) {
-                                    fetch(data.results[j].films[k])
-                                    .then(function (response) {
-                                            return response.json()
-                                    }).then(function (films) {
-                                        console.log(films)
-                                
-                                    })
-                                }
-
-                                
+                                // for(var k = 0; k < data.results[j].films.length; k++) {
+                                //     fetch(data.results[j].films[k])
+                                //     .then(function (response) {
+                                //             return response.json()
+                                //     }).then(function (films) {
+                                //         console.log(films);
+                                //     })
+                                // }
+ 
                             }
                         }
                     }
@@ -91,16 +95,16 @@ function init() {
     }
 
     function hideHero(){
-        hero.classList.remove('display-hero')
-        hero.classList.add('hide')  
+        hero.classList.remove('display-hero');
+        hero.classList.add('hide');
     }
 
     function previousPage() {
-            if (count > 1) {
-                next.classList.remove('ends')
-                --count
+            if (count >= 1) {
+                next.classList.remove('ends');
+                --count;
             } else {
-                previous.classList.add('ends')
+                previous.classList.add('ends');
             }
         url = `https://swapi.dev/api/people/?page=${count}`;
         fetch(url)
@@ -108,9 +112,8 @@ function init() {
                 return resp.json();
             }).then(function (data) {
                 for (var i = 0; i < data.results.length; i++) {
-                    showPerson[i].innerHTML = data.results[i].name
-
+                    showPerson[i].innerHTML = data.results[i].name;
                 }
             })
-        }
+    }
 }
